d7ee98857b67cbb39cfebcc9b7daf1b07b92b38c
110hustlehouse-code <110hustlehouse@gmail.com>
2026-09-04 18:38:29 +0000

    chore: cleanup - remove Playwright, test files, unused deps

--- src/test-send.ts
+++
@@ -1,9 +0,0 @@
-import { listAccounts } from './core/of-client/auth.js';
-import 'dotenv/config';
-
-async function test() {
-  const accounts = await listAccounts();
-  console.log('Accounts:', JSON.stringify(accounts, null, 2));
-}
-
-test();
